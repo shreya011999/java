c991a32e8e637d89c7243e16450dbfafade05bfd
shreya011999 <73688917+shreya011999@users.noreply.github.com>
2020-11-01 10:18:06 +0530

    Add files via upload

---
+++ ass4a.java
@@ -0,0 +1,16 @@
+abstract class Ass4
+{
+	public static void show()
+	{
+		System.out.println("Abstract class");
+	}
+}
+class Ass4a 
+{
+	public static void main(String args[])
+	{
+		Ass4.show();
+	}
+}
+	
+
